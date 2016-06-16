b9d3b67c536a5548167bae3ba3e4edb7f4894754
A-Reum Jung <areumjung1987@gmail.com>
2016-06-16 09:08:01 -0600

    1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const
 , app     = express()
 
 app
-.use(express.static(__dirname))
-.get('/cv', (req, res) => res.sendFile(__dirname) + '/dist')
+.use(express.static(__dirname + "/dist"))
+.get('/cv', (req, res) => res.sendFile(__dirname))
 .use((req, res) => res.sendFile(__dirname + '/404/index.html'))
 .listen(port, () => console.log(`wikipedia-viewer listening on ${port}`))
